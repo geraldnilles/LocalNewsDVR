865d5e58de7538fd5bb23e117fe87e856dec7f2e
Gerald Nilles <gerald@nilleye.com>
2022-06-01 14:29:07 -0700

    Add CORS library

--- localnews/__init__.py
+++ localnews/__init__.py
@@ -4,8 +4,11 @@ from flask import Flask
 from flask import render_template
 from flask import send_from_directory
 
+from flask_cors import CORS
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
 
     app.config.from_mapping(
         SECRET_KEY='dev',
