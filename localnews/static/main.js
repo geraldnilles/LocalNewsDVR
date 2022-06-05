14e7e420d4fff481896b859aa1a9e1d9b06908b2
Gerald Nilles <gerald@nilleye.com>
2022-06-05 14:48:05 -0700

    Add ability to change chromecast device

--- localnews/static/main.js
+++ localnews/static/main.js
@@ -7,7 +7,7 @@ function bind_show(){
             // Remove the on-click for now so that we cant add more buttons
             var value = e.target.closest("button").innerText;
             // var count = document.querySelector("input.episodeCount").value;
-            send_request("play/"+value+"/Living Room TV");
+            send_request("play/"+value+"/"+get_device_name());
         }
     }
 }
@@ -26,9 +26,27 @@ function send_request(url){
     
 }
 
+function get_device_name(){
+    return document.querySelector("div.btn-group button.active").innerText;
+}
+
+function bind_toggle(){
+    var bg = document.querySelector("div.btn-group");
+    var buttons = bg.querySelectorAll("button");
+    buttons.forEach(function(b){
+        b.onclick = function(e){
+            buttons.forEach(function(a){
+                a.classList.remove("active"); 
+            });
+            e.target.classList.add("active");
+        }
+    });
+}
+
 
 function bind_buttons(){
     bind_show();
+    bind_toggle();
 }
 
 bind_buttons();
